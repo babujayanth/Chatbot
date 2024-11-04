160b7181bd6b7d354793b3da639e3cb8356b8c1d
babujayanth <34885646+babujayanth@users.noreply.github.com>
2024-11-04 14:57:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,15 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 import tensorflow as tf 
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Restrict TensorFlow to only the first GPU
+        tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+        tf.config.experimental.set_memory_growth(gpus[0], True)
+    except RuntimeError as e:
+        print(e)
+
 import re
 from nltk.corpus import stopwords
 
